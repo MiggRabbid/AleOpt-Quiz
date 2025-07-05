7a41f8bbd9a08b07cdcde7d316c83e6e94307680
MiggRabbid <mig26@inbox.ru>
2025-07-05 14:43:55 +0300

    Update: BtnAdd

--- frontend/src/layouts/AdminPage/features/AdminStats/components/Header/ui/BtnAdd.tsx
+++ frontend/src/layouts/AdminPage/features/AdminStats/components/Header/ui/BtnAdd.tsx
@@ -1,9 +1,16 @@
 import { BtnSmall } from '@/shared/ui/ui/btns/BtnSmall';
-import { TypeStatsTab } from '../../../types/AdminStats';
 
-const BtnAdd = ({ activeTab }: { activeTab: TypeStatsTab }) => {
+import { TypeStatsTab } from '@/layouts/AdminPage/types/AdminStats';
+
+interface IBtnAddProps {
+  activeTab: TypeStatsTab;
+  openEditor: () => void;
+}
+
+const BtnAdd = (props: IBtnAddProps) => {
+  const { activeTab, openEditor } = props;
   const btnText = `Добавить ${activeTab === TypeStatsTab.users ? 'сотрудника' : 'вопрос'}`;
-  return <BtnSmall btnText={btnText} variant="outlined" />;
+  return <BtnSmall btnText={btnText} variant="outlined" btnClick={openEditor} />;
 };
 
 export { BtnAdd };
