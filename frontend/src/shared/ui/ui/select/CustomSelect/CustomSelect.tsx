713194b0031ac23f2b7ee04655f8c097b329e8e2
MiggRabbid <mig26@inbox.ru>
2025-07-05 14:42:47 +0300

    Add: CustomSelect

---
+++ frontend/src/shared/ui/ui/select/CustomSelect/CustomSelect.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import { Box, InputLabel, MenuItem, Select } from '@mui/material';
+
+import type { SelectChangeEvent } from '@mui/material';
+
+export interface ICustomSelectItem {
+  value: string | number;
+  text: string;
+}
+
+export type TCustomSelectItems = Array<ICustomSelectItem>;
+
+interface ICustomSelectProps {
+  label: string;
+  value: string | undefined;
+  // eslint-disable-next-line no-unused-vars
+  onChange: (event: SelectChangeEvent) => void;
+  items: TCustomSelectItems;
+  error?: any;
+}
+
+const CustomSelect = (props: ICustomSelectProps) => {
+  const { label, value, onChange, items, error } = props;
+
+  return (
+    <Box className="relative h-fit w-full">
+      <InputLabel id="custom-select-label" className="w-full" color="success">
+        {label}
+      </InputLabel>
+      <Select
+        labelId="custom-select-label"
+        id="custom-select"
+        value={value}
+        label={label}
+        error={!!error}
+        onChange={onChange}
+        className="w-full rounded-xl!"
+        color="success"
+      >
+        {items.map((item, index: number) => (
+          <MenuItem key={`custom-select-item-key${index}`} value={item.value}>
+            {item.text}
+          </MenuItem>
+        ))}
+      </Select>
+    </Box>
+  );
+};
+
+export default React.memo(CustomSelect);
---
+++ frontend/src/shared/ui/ui/select/CustomSelect/index.ts
@@ -0,0 +1,5 @@
+import CustomSelect from './CustomSelect';
+import type { ICustomSelectItem, TCustomSelectItems } from './CustomSelect';
+
+export { CustomSelect };
+export type { ICustomSelectItem, TCustomSelectItems };
