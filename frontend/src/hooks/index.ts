4fe228e79998a8e5166990e8ca8238f7ff5ac7cf
MiggRabbid <mig26@inbox.ru>
2025-07-05 14:43:27 +0300

    Add: useDebounce

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,7 +1,9 @@
 import { useAppSelector } from './useAppSelector';
 import { useAppActions } from './useAppActions';
 import { useLocalStorage } from './useLocalStorage';
+import { useDebounce } from './useDebounce';
 
 export { useAppSelector };
 export { useAppActions };
 export { useLocalStorage };
+export { useDebounce };
