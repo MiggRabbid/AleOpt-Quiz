ddd627916996fbd6196e10a441a3e0a890f76771
MiggRabbid <mig26@inbox.ru>
2025-07-05 13:19:45 +0300

    Add: BtnGroup

---
+++ frontend/src/shared/ui/ui/btns/BtnGroup.tsx
@@ -0,0 +1,67 @@
+import { Box, Button, CircularProgress } from '@mui/material';
+
+type btnVariant = 'text' | 'contained' | 'outlined';
+type btnColor =
+  | 'success'
+  | 'inherit'
+  | 'primary'
+  | 'secondary'
+  | 'error'
+  | 'info'
+  | 'warning';
+type btnType = 'button' | 'reset' | 'submit';
+
+interface IBtnGroupProps {
+  leftBtnText: string;
+  leftBtnClick?: () => void;
+  leftBtnVariant?: btnVariant;
+  leftBtnColor?: btnColor;
+  rightBtnText: string;
+  rightBtnClick?: () => void;
+  rightBtnVariant?: btnVariant;
+  rightBtnColor?: btnColor;
+  rightBtnType?: btnType;
+  isLoading?: boolean;
+}
+
+const BtnGroup = (props: IBtnGroupProps) => {
+  const {
+    isLoading,
+    leftBtnText,
+    leftBtnClick,
+    leftBtnColor,
+    leftBtnVariant,
+    rightBtnText,
+    rightBtnClick,
+    rightBtnColor,
+    rightBtnVariant,
+    rightBtnType,
+  } = props;
+
+  return (
+    <Box className="flex w-full items-center gap-x-5 gap-y-2">
+      <Button
+        onClick={leftBtnClick}
+        variant={leftBtnVariant}
+        color={leftBtnColor}
+        disabled={isLoading}
+        className="min-h-10! flex-1! rounded-xl!"
+      >
+        {leftBtnText}
+      </Button>
+      <Button
+        onClick={rightBtnType === 'submit' ? undefined : rightBtnClick}
+        variant={rightBtnVariant}
+        color={rightBtnColor}
+        type={rightBtnType}
+        disabled={isLoading}
+        className="min-h-10! flex-1! items-center justify-center gap-2 rounded-xl!"
+      >
+        {isLoading && <CircularProgress sx={{ color: 'white !important' }} size={30} />}
+        {rightBtnText}
+      </Button>
+    </Box>
+  );
+};
+
+export { BtnGroup };
