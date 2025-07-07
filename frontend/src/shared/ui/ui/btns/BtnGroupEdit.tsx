2a47068887c32fbf67864a385786472c16e21171
MiggRabbid <mig26@inbox.ru>
2025-07-07 11:53:33 +0300

    Add: create BtnGroupEdit

---
+++ frontend/src/shared/ui/ui/btns/BtnGroupEdit.tsx
@@ -0,0 +1,51 @@
+import { Box, IconButton } from '@mui/material';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditSquareIcon from '@mui/icons-material/EditSquare';
+
+type TBtnColor =
+  | 'success'
+  | 'inherit'
+  | 'primary'
+  | 'secondary'
+  | 'error'
+  | 'info'
+  | 'warning';
+
+type TBtnSize = 'small' | 'medium' | 'large';
+
+interface IBtnGroupEditProps {
+  // eslint-disable-next-line no-unused-vars
+  onClickEdit: (e: React.MouseEvent) => void;
+  // eslint-disable-next-line no-unused-vars
+  onClickDelete: (e: React.MouseEvent) => void;
+  colorEdit?: TBtnColor;
+  colorDelete?: TBtnColor;
+  size?: TBtnSize;
+}
+
+const BtnGroupEdit = (props: IBtnGroupEditProps) => {
+  const { onClickEdit, onClickDelete, colorEdit, colorDelete, size } = props;
+  return (
+    <Box className="me-4 flex w-fit items-center gap-2">
+      <IconButton
+        color={colorEdit}
+        size={size}
+        onClick={onClickEdit}
+        id="btn-group-edit__edit"
+      >
+        <EditSquareIcon />
+      </IconButton>
+      <IconButton
+        color={colorDelete}
+        size={size}
+        onClick={onClickDelete}
+        id="btn-group-edit__delete"
+      >
+        <DeleteIcon />
+      </IconButton>
+    </Box>
+  );
+};
+
+export { BtnGroupEdit };
--- frontend/src/shared/ui/ui/btns/index.ts
+++ frontend/src/shared/ui/ui/btns/index.ts
@@ -1,5 +1,6 @@
 import { BtnGroup } from './BtnGroup';
+import { BtnGroupEdit } from './BtnGroupEdit';
 import { BtnMain } from './BtnMain';
 import { BtnSmall } from './BtnSmall';
 
-export { BtnGroup, BtnMain, BtnSmall };
+export { BtnGroup, BtnMain, BtnSmall, BtnGroupEdit };
